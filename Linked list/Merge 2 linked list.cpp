4c4b05a2e8663ec28e9e4f0b0713e524d939b0ed
Satyam <satyam2507mishra@gmail.com>
2020-11-26 00:08:12 +0530

    Final commit

---
+++ Linked list/Merge 2 linked list.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* third, *last;
+        
+        if(l1->val<l2->val)
+        {
+            third = last = l1;
+            l1 = l1->next;
+            last->next = NULL;
+
+        }
+        else
+        {
+            third = last = l2;
+            l2 = l2->next;
+            last->next = NULL;   
+        }
+        while(l1!=NULL && l2!=NULL)
+        {
+                if(l1->val<l2->val)
+                {
+                        last->next = l1;
+                        last = l1;
+                        l1 = l1->next;
+                        last->next =NULL;
+                }
+                else
+                {
+                        last->next = l2;
+                        last = l2;
+                        l2 = l2->next;
+                        last->next =NULL;
+                }
+            if(l1!=NULL) 
+                last->next = l1;
+            else
+                last->next = l2;
+            
+            
+        }
+        
+        
+    }
+};
