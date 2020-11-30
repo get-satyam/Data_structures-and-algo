0b07042ae756e56cddce4c0a4918cb9ea0f69245
Satyam <satyam2507mishra@gmail.com>
2020-11-30 15:58:43 +0530

    Creatiing a stack

---
+++ #05 Stack and it's operations/Creation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+struct stack {
+    int size;
+    int top;
+    int * s;
+}
+t;
+
+using namespace std;
+
+int main() {
+    cout << "Enter the size of the stack: ";
+    cin >> t.size;
+    t.s = new int[t.size];
+    return 0;
+}
